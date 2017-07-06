4f730410c60962bd32e7e31ad3869795ace2930f
luu-alex <luu.alex@gmail.com>
2017-07-06 16:45:52 +0000

    new public styles

---
+++ public/scripts/scroll.js
@@ -0,0 +1,13 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+})(jQuery);
